3917ea4ae8dde5fea9c4fb58ddb746467f6faee4
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-17 10:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function volume_sphere() {
 	let radius = document.getElementByid("radius");
-	let volume = document.getElementByid("volume0");
+	let volume = document.getElementByid("volume");
 	volume = radius * radius;
 	return volume;
 	
 } 
+console.log(volume_sphere());
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
